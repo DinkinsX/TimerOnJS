fff9e37ebd805a782a2581f4196e8d13dbba830f
DinkinsX <dan767@yandex.ru>
2020-07-31 18:59:53 +0700

    Revert "First commit"
    This reverts commit bbfdccac97230a3c32b0df884d358e6423bf88ea.

--- js/script.js
+++ js/script.js
@@ -1,50 +1,42 @@
-//Timer
-let deadline = '2020-08-01'; // до какого числа таймер должен работать
-window.addEventListener('DOMContentLoaded', function() { //сработает после прогрузки древа html
-    function getTimeRemaning(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()), //millisec отнимаем указанную дату от нынешней
-        seconds = Math.floor((t / 1000 ) % 60),
-        minutes = Math.floor((t / 1000 / 60) % 60),
-        hours = Math.floor(t / 1000 / 60 / 60);
-        //days = Math.floor(t / 1000 / 60 / 60 / 24);
-        return {
-            'total' : t,
-            'hours' : hours,
-            'minutes' : minutes,
-            'seconds' : seconds
-        };
+let blockTabs = '.info-header',
+    singleTab = '.info-header-tab',
+    contentTab = '.info-tabcontent';
+
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict'; 
+
+    let tab = document.querySelectorAll(singleTab),
+        info = document.querySelector(blockTabs), //parent
+        tabContent = document.querySelectorAll(contentTab);
+
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+
     }
+    hideTabContent(1); // how many tabs show on start
 
-    function setClock(id, endtime) {  // берем элементы со страницы по id
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-        function updateClock() { // получает разницу во времени и записываем в вёрстку
-            let t = getTimeRemaning(endtime);
-            if (+t.hours < 10) {
-                hours.textContent =  '0' + t.hours;
-            }  else {
-                hours.textContent = t.hours;
-            }
-            if (+t.minutes < 10) {
-                minutes.textContent = '0' + t.minutes;
-            } else {
-                minutes.textContent = t.minutes;
-            }   
-            if (+t.seconds < 10) { 
-                seconds.textContent = '0' + t.seconds;
-            } else {
-                seconds.textContent = t.seconds;
-            }
-            
-            if (t.total <= 0) { // стоптаймер
-                clearInterval(timeInterval);
-            }
-
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) { //check hide
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
         }
     }
-    setClock('timer', deadline);
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains(singleTab.slice(1))) { 
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
 });
